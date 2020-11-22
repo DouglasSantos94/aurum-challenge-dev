0a4954aa7e19d982c10f792d19f4c9e2175d74b0
douglas <dgstos94@gmail.com>
2020-11-22 18:13:34 -0300

    check first and last char

--- replaceChar.js
+++ replaceChar.js
@@ -1,38 +1,32 @@
+const getLastValidChar = (string) => {
+    while (string[string.length-1] == '?') {
+        string = string.slice(0, string.length-1);
+    }
+    return string[string.length-1];
+}
+
+const firstChar = (index) => index == 0;
+
 /**
  * @param {string} string  
  * @param {number} times 
  * @return {string}  
  */
-
-// recebe uma string e um int
-// retorna uma string
-// se encontrar o caracter ?, substituir pelo caracter que está antes dele; deve criar uma repetição com o numero de vezes indicado no segundo parametro;
-// se ? for o primeiro da string, deve ser colocado o ultimo caracter da string passada como parametro
 const replaceChar = (string, times) => {
-    let formatedString = '';
+    let formattedString = '';
     let index = 0;
     let latestChar = '';
+    let aux = string;
     while (index < string.length) {
         if (string[index] == '?') {
-            if (index == 0) {
-                if (string[string.length-1] == '?') {
-                    formatedString += string[string.length-2].repeat(times);
-                } else {
-                    formatedString += string[string.length-1].repeat(times)
-                }
-            } else {
-                formatedString += latestChar.repeat(times)
-            }
+            formattedString += firstChar(index) ? getLastValidChar(aux) : latestChar.repeat(times);
         } else {
-            formatedString += string[index];
+            formattedString += string[index];
+            latestChar = string[index];
         }
-        latestChar = string[index];
-        index++
+        index++;
     }
-    return formatedString;
+    return formattedString;
 }
-// corrigir bug quando o ultimo caracter é ?
-console.log(replaceChar('?x', 3));
-console.log(replaceChar("x?yzza?y?", 1));
-console.log(replaceChar("x?yzza?y?", 2));
-console.log(replaceChar("?x?yzza?y?", 2));
+
+export default replaceChar;
